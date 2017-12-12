873c41b4f5eb9ec3e53bfa7a4bade98ab70802ce
Chris Cao <ccao@cmsonline.com>
2017-12-12 21:09:15 +0800

    android commit

---
+++ app/src/main/java/com/bluebamboo/k11demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bluebamboo.k11demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
